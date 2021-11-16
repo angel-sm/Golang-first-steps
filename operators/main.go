4ab1fac9240778250c6e6f31053118b716f23fe5
angel-sm <josesanchez@solemti.mx>
2021-11-16 14:00:44 -0600

    (Change): change folder structure

--- operators/main.go
+++ operators/main.go
@@ -6,38 +6,6 @@ import (
 )
 
 func main() {
-	// Declarar constantes
-
-	const pi float64 = 3.14
-	const pi2 = 3.14
-
-	fmt.Println("pi", pi)
-	fmt.Println("pi2", pi2)
-
-	// Declarar variables
-
-	age := 18 // Dos puntos se usan cuando la variablo no esta creada entonces se creara y se le asignara el valor y parsea el tipo de dato
-	var area int = 10
-	var area2 = 10
-
-	fmt.Println(age, area, area2)
-
-	// Zero values
-	var a int     // enteras retorna 0
-	var b float64 // decimales retorna 0
-	var c string  // texto retorna empty
-	var d bool    // true o false retorna false
-
-	fmt.Println(a, b, c, d)
-
-	// area de un cuadrado
-	const base float64 = 10.5
-	const altura float64 = 10.5
-
-	areaCuadrado := base * altura
-
-	fmt.Println("area:", areaCuadrado)
-
 	// Operadores aritmeticos
 
 	x := 10
@@ -90,5 +58,4 @@ func main() {
 	const radioCirculo = 19
 	areaCirculo := math.Pi * math.Pow(radioCirculo, 2)
 	fmt.Println("Area Circulo:", areaCirculo)
-
 }
